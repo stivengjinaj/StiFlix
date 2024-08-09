5a29fb7537893fbd5c2a16f82fa60e8f0c26590f
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-09 22:28:16 +0200

    Added movie playing route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import InitialPage from "./pages/InitialPage/InitialPage.jsx";
 import Movies from "./pages/Movies/Movies.jsx";
+import MoviePlaying from "./pages/MoviePlaying/MoviePlaying.jsx";
 
 function App() {
     return (
         <Routes>
             <Route index element={<InitialPage />} />
             <Route path={'/movies'} element={<Movies />} />
+            <Route path={'/movies/:movieId'} element={<MoviePlaying/>} />
         </Routes>
     );
 }
