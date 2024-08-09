503915dd179a5de43be7a1b0a13e7aa564b9427c
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-09 13:07:49 +0200

    Added random number generator and string slicer function.

--- src/helper/miscs.js
+++ src/helper/miscs.js
@@ -1,3 +1,4 @@
+
 export function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
     return {
@@ -5,3 +6,21 @@ export function getWindowDimensions() {
         height
     };
 }
+
+export function sliceStr(str) {
+    if (str.length >= 5) {
+        return str.slice(0, 5).toLowerCase();
+    } else {
+        return str.toLowerCase(); // Or return a default value if needed
+    }
+}
+
+export function rand(min, max) {
+    // Ensure min is less than or equal to max
+    if (min > max) {
+        [min, max] = [max, min]; // Swap min and max if necessary
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
