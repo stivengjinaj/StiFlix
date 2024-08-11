d226f0632d8b9279a9fd01ca72cbab38f2bc6525
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-12 01:05:17 +0200

    Fetched movie object

---
+++ src/models/FetchedMovie.mjs
@@ -0,0 +1,61 @@
+class FetchedMovie {
+    constructor(jsonMovie, isSeries, seasons, episodes) {
+        this.id = jsonMovie.id;
+        this.title = jsonMovie.original_title;
+        this.overview = jsonMovie.overview;
+        this.backdrop_path = jsonMovie.backdrop_path;
+        this.poster_path = jsonMovie.poster_path;
+        this.genres_ids = jsonMovie.genre_ids;
+        this.release_date = jsonMovie.release_date;
+        this.vote_average = jsonMovie.vote_average;
+        this.isSeries = isSeries;
+        this.seasons = seasons;
+        this.episodes = episodes;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getOverview() {
+        return this.overview;
+    }
+
+    getBackdropPath() {
+        return this.backdrop_path;
+    }
+
+    getPosterPath() {
+        return this.poster_path;
+    }
+
+    getGenresIds() {
+        return this.genres_ids;
+    }
+
+    getReleaseDate() {
+        return this.release_date;
+    }
+
+    getVoteAverage() {
+        return this.vote_average;
+    }
+
+    getMediaType() {
+        return this.isSeries ? 'tv' : 'movie';
+    }
+
+    getSeasons() {
+        return this.seasons;
+    }
+
+    getEpisodes() {
+        return this.episodes;
+    }
+}
+
+export default FetchedMovie;
