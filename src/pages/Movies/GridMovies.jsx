2a196a4a4d16ba2d943770173424576a999b756d
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-12 15:36:40 +0200

    Added grid system to view either movies or tv shows

---
+++ src/pages/Movies/GridMovies.jsx
@@ -0,0 +1,33 @@
+import {useGSAP} from "@gsap/react";
+
+{/* eslint-disable react/prop-types */}
+import {Card, Col, Container, Row} from "react-bootstrap";
+import {gsap} from "gsap";
+
+function GridMovies(props) {
+    useGSAP(() => {
+        gsap.from('.allMovies', {
+            y: 100,
+            opacity: 0,
+        })
+    }, []);
+    return (
+        <Container fluid className="py-5 allMovies">
+            <Row>
+                {props.movies && props.movies.map(movie => (
+                    <Col xs={12} sm={6} md={4} lg={2} key={movie.id} className="mb-4">
+                        <Card className="h-100 border-0">
+                            <Card.Img
+                                variant="top"
+                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                alt={movie.title}
+                            />
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+}
+
+export default GridMovies;
