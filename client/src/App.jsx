f1cd9a46d76c4293bb893fe3bda76d2101acdf09
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-14 18:28:41 +0200

    Added Login and Register routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import InitialPage from "./pages/InitialPage/InitialPage.jsx";
 import Movies from "./pages/Movies/Movies.jsx";
 import MoviePlaying from "./pages/MoviePlaying/MoviePlaying.jsx";
-import Login from "./pages/Login/Login.jsx";
+import Login from "./pages/Authentication/Login.jsx";
+import Register from "./pages/Authentication/Register.jsx";
 
 function App() {
     return (
         <Routes>
             <Route index element={<InitialPage />} />
             <Route path={'/movies'} element={<Movies />} />
-            <Route path={'/movies/:movieId'} element={<MoviePlaying/>} />
+            <Route path={'/movies/:movieId'} element={<MoviePlaying />} />
             <Route path={'/login'} element={<Login />} />
+            <Route path={'/register'} element={<Register />} />
         </Routes>
     );
 }
