4d29717c3948a02af1d1931575dbca262785a4f8
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-18 16:27:19 +0200

    Added routes to profile dropdown

--- client/src/pages/InitialPage/InitialPage.jsx
+++ client/src/pages/InitialPage/InitialPage.jsx
@@ -78,6 +78,9 @@ function InitialPage() {
                                         <Dropdown.Menu className="mt-2 bg-dark">
                                             <Dropdown.Item className="text-white" href="/movies">Home</Dropdown.Item>
                                             <Dropdown.Item className="text-white" href="/account">Account</Dropdown.Item>
+                                            <Dropdown.Item className="text-white" href="/favourites">Favourites</Dropdown.Item>
+                                            <Dropdown.Item className="text-white" href="/watched-list">Watched List</Dropdown.Item>
+                                            <Dropdown.Item className="text-white" href="/to-watch-list">To Watch List</Dropdown.Item>
                                             <Dropdown.Item onClick={handleSignOut} className="text-danger">Logout</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
