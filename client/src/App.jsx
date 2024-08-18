3d6aff968a99b016c3c45375e967b689a75195a0
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-18 16:27:01 +0200

    Added movie info and movie playing route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import InitialPage from "./pages/InitialPage/InitialPage.jsx";
 import Movies from "./pages/Movies/Movies.jsx";
-import MoviePlaying from "./pages/MoviePlaying/MoviePlaying.jsx";
 import Login from "./pages/Authentication/Login.jsx";
 import Register from "./pages/Authentication/Register.jsx";
+import MovieDetails from "./pages/MoviePlaying/MovieDetails.jsx";
+import MoviePlaying from "./pages/MoviePlaying/MoviePlaying.jsx";
 
 function App() {
     return (
         <Routes>
             <Route index element={<InitialPage />} />
             <Route path={'/movies'} element={<Movies />} />
+            <Route path={'/movies/info/:movieId'} element={<MovieDetails />} />
             <Route path={'/movies/:movieId'} element={<MoviePlaying />} />
             <Route path={'/login'} element={<Login />} />
             <Route path={'/register'} element={<Register />} />
