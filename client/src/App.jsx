5d2c3cb5f2016c50579c3268eeb48a2fe248d048
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-08-26 11:28:45 +0200

    Added user state control from the root. Blocked some routes if user is not logged in.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import './App.css';
+import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import InitialPage from "./pages/InitialPage/InitialPage.jsx";
@@ -10,22 +10,61 @@ import MovieDetails from "./pages/MoviePlaying/MovieDetails.jsx";
 import MoviePlaying from "./pages/MoviePlaying/MoviePlaying.jsx";
 import MyAccount from "./pages/Account/MyAccount.jsx";
 import PersonalMovies from "./pages/Personal/PersonalMovies.jsx";
+import {useEffect, useState} from "react";
+import {auth} from "../firebaseConfiguration.js";
+import Loading from "./pages/Movies/Loading.jsx";
 
 function App() {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((usr) => {
+            setUser(usr);
+            setLoading(false);
+        });
+        return () => unsubscribe(); // Cleanup subscription on unmount
+    }, []);
+
+    const handleSignOut = async (e) => {
+        e.preventDefault();
+        await auth.signOut().then(() => {
+            setUser(null);
+            navigate("/");
+        });
+    };
+
+    if (loading) {
+        return <Loading />;
+    }
+
     return (
         <Routes>
-            <Route index element={<InitialPage />} />
+            <Route index element={<InitialPage user={user} handleSignOut={handleSignOut}/>} />
             <Route path={'/movies'} element={<Movies />} />
             <Route path={'/movies/info/:movieId'} element={<MovieDetails />} />
             <Route path={'/movies/:movieId'} element={<MoviePlaying />} />
-            <Route path={'/login'} element={<Login />} />
-            <Route path={'/register'} element={<Register />} />
-            <Route path={'/account'} element={<MyAccount />} />
-            <Route path={'/favourites'} element={<PersonalMovies type={'favourites'} />} />
-            <Route path={'/watchLater'} element={<PersonalMovies type={'watchLater'} />} />
-            <Route path={'/watchlist'} element={<PersonalMovies type={'watchlist'} />} />
+            <Route path={'/login'} element={
+                !user ? <Login /> : <Navigate to={'/'} />
+            } />
+            <Route path={'/register'} element={
+                !user ? <Register /> : <Navigate to={'/'} />
+            } />
+            <Route path={'/account'} element={
+                user ? <MyAccount /> : <Navigate to={'/login'} />
+            } />
+            <Route path={'/favourites'} element={
+                user ? <PersonalMovies type={'favourites'} /> : <Navigate to={'/login'} />
+            } />
+            <Route path={'/watchLater'} element={
+                user ? <PersonalMovies type={'watchLater'} /> : <Navigate to={'/login'} />
+            } />
+            <Route path={'/watchlist'} element={
+                user ? <PersonalMovies type={'watchlist'} /> : <Navigate to={'/login'} />
+            } />
         </Routes>
     );
 }
 
-export default App
+export default App;
