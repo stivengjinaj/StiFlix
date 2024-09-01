71dcd7e506ca82bcc29b9c721c2da2f3d34a39c5
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-09-01 11:28:17 +0200

    Added UI for movies in progress

---
+++ client/src/models/MovieInProgress.js
@@ -0,0 +1,10 @@
+class MovieInProgress {
+  constructor(movie, season, episode, date) {
+    this.movie = movie;
+    this.season = season;
+    this.episode = episode;
+    this.date = date;
+  }
+}
+
+export default MovieInProgress;
---
+++ client/src/pages/Movies/ContinueWatching.jsx
@@ -0,0 +1,40 @@
+import {useNavigate} from "react-router-dom";
+
+{/* eslint-disable react/prop-types */}
+import {Container, Row} from "react-bootstrap";
+
+function ContinueWatching (props) {
+    const navigate = useNavigate();
+    return (
+        <Container fluid className="p-0 mt-3">
+            <Row className="justify-content-start align-items-center px-3 mt-3">
+                <h3 style={{ fontFamily: 'Netflix Sans1' }} className="text-white mt-3">
+                    Continue Watching
+                </h3>
+            </Row>
+            <Container fluid className="w-100 mt-3 no-scrollbar overflow-x-scroll">
+                <table>
+                    <tbody>
+                        <tr>
+                            {
+                                props.movies.map((object, index) => (
+                                    <td key={index} className="carousel-slider-cover mx-2" onClick={() =>
+                                        navigate(`/${object.movie.isSeries ? "tv" : "movie"}/${object.movie.id}/${object.season}/${object.episode}`)}>
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w500/${object.movie.poster_path}`}
+                                            alt={object.movie.title}
+                                            className="carousel-image"
+                                        />
+                                    </td>
+                                ))
+                            }
+                        </tr>
+                    </tbody>
+                </table>
+            </Container>
+        </Container>
+    );
+
+}
+
+export default ContinueWatching;
