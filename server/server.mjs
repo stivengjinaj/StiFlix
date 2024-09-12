e0ef14a7662b4cfc01740975ca67e922b34aa845
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-09-12 23:22:00 +0200

    Implemented cors test fix

--- server/server.mjs
+++ server/server.mjs
@@ -499,6 +499,17 @@ app.use('/api/getMovieIdBraflix', createProxyMiddleware({
     }
 }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
 
 app.listen(3000, () => {
     console.log('Proxy server is online on port 3000');
