a32b95598f2a5142d405bb4846fb73b9a13528c5
Stiven Gjinaj <stivengjinaj@hotmail.com>
2025-12-25 23:55:06 +0100

    Fix: fixed not found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,17 +1,22 @@
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 function NotFound() {
+    const navigate = useNavigate();
     return (
-        <Container fluid className="p-0 min-vh-100 bg-gradient-dark-radius align-items-center py-5">
-            <h1 className="text-danger text-center mt-5">
+        <Container fluid className="p-0 min-vh-100 bg-gradient-dark-radius d-flex flex-column justify-content-center align-items-center py-5">
+            <h1 className="text-danger text-center">
                 404
             </h1>
-            <h1 className="text-white text-center mt-5">
-               Hold on bad boy,<br/> this page doesn't exist.
+            <h1 className="text-white text-center mt-2">
+                {`This page doesn't exist`}
             </h1>
-            <h1 className="text-center mt-5">
-                &#128536;
-            </h1>
+            <Button
+                className="sc_button mt-5"
+                onClick={() => navigate("/movies")}
+            >
+                Back to the movies
+            </Button>
         </Container>
     );
 }
